d2bf1f5c0f5efaf2ea20f66ed42e305f35202270
yangli <1004052263@qq.com>
2015-10-16 15:51:45 +0800

    完成自动更新天气和切换城市功能

--- src/com/weather/app/activity/WeatherActivity.java
+++ src/com/weather/app/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.coolweather.app.R;
+import com.weather.app.service.AutoUpdateService;
 import com.weather.app.util.HttpCallbackListener;
 import com.weather.app.util.HttpUtil;
 import com.weather.app.util.Utility;
@@ -166,6 +167,8 @@ publishText.setText("今天" + prefs.getString("publish_time", "") + "发布");
 currentDateText.setText(prefs.getString("current_date", ""));
 weatherInfoLayout.setVisibility(View.VISIBLE);
 cityNameText.setVisibility(View.VISIBLE);
+Intent intent = new Intent(this, AutoUpdateService.class);
+startService(intent);
 }
 }
 
